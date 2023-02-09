9c911614432a3ee85dcb3d15659ef15e4447f6aa
Bryton Hall <email@bryton.io>
2023-02-08 21:51:20 -0500

    switch to nix flakes and add direnv

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = inputs@{ self, ... }:
+    inputs.utils.lib.eachDefaultSystem (system:
+      let pkgs = inputs.nixpkgs.legacyPackages.${system}; in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            nodejs
+            zip
+          ];
+        };
+      }
+    );
+}
